cd71556ae9bf9089c370a465720ea442d8a85fba
FantasticApple <zhawhjw@users.noreply.github.com>
2025-02-17 02:12:15 -0500

    update:NMR->zhawhjw

--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ setup(
             "pytube @ git+https://github.com/pytube/pytube.git",
             "pyopengl @ git+https://github.com/mmatl/pyopengl.git",
             "chumpy @ git+https://github.com/mattloper/chumpy", # smplx dependency
-            "neural-renderer-pytorch @ git+https://github.com/shubham-goel/NMR.git",
+            "neural-renderer-pytorch @ git+https://github.com/zhawhjw/NMR.git",
         ],
     extras_require={
         'all': [
